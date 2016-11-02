89940648714bad65a55e837af4695fd8fb968455
Juanita Soranno <juanitasoranno@gmail.com>
2016-11-02 18:38:03 -0400

    Added notes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,3 +1,17 @@
 /**
  * Created by juanitasoranno on 10/27/16.
  */
+module.exports = function(app) {
+
+
+    app.use('/', function (req, res) {
+        //path for html page to render here
+    });
+
+    app.listen(PORT, function () {
+        console.log('App listening on PORT ' + PORT);
+    });
+
+};
+
+//remember to save yourself to the array AFTER difference
--- app/server.js
+++ app/server.js
@@ -1,9 +1,7 @@
 /**
  * Created by juanitasoranno on 10/27/16.
  */
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
+var path = require("path");
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -14,3 +12,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require("./app/routing/api-routes.js")(app);
